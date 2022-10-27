cc0c41de760ca0b8997d7c50bef2c6ea321301c6
Sergei <395105@mail.ru>
2022-10-27 16:27:11 +0400

    -Util конфигурация для Hibernate настроен без xml файла

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Util.getSessionFactory();
+        Util.getSessionFactory().openSession();
         UserService impl = new UserServiceImpl();
         impl.createUsersTable();
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -9,27 +11,43 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-    //hibernate код
-    private static SessionFactory sessionFactory;
-    static {
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure()
-                .build();
-        try {
-            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        } catch (Exception e) {
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
-    }
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-    //jdbc код
     private final static String URL = "jdbc:mysql://localhost:3306/mydbstest";
     private final static String USERNAME = "root";
     private final static String PASSWORD = "123456as";
+    //hibernate код
+    private static final SessionFactory concreteSessionFactory;
+    static {
+        try {
+            Properties prop = new Properties();
+            prop.setProperty("hibernate.connection.url", URL);
+            prop.setProperty("hibernate.connection.username", USERNAME);
+            prop.setProperty("hibernate.connection.password", PASSWORD);
+            prop.setProperty("hibernate.show_sql","true");
+            prop.setProperty("hibernate.default_schema","mydbstest");
+            prop.setProperty("hibernate.hibernate.format_sql","true");
+            prop.setProperty("hibernate.hibernate.current_session_context_class", "thread");
+
+            prop.setProperty("hibernate.hbm2ddl.auto", "create");
+
+            concreteSessionFactory = new org.hibernate.cfg.Configuration()
+                    .addProperties(prop)
+                    //.addPackage("com.kat")
+                    .addAnnotatedClass(jm.task.core.jdbc.model.User.class)
+                    .buildSessionFactory()
+            ;
+        }
+        catch (Exception ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public static SessionFactory getSessionFactory() throws HibernateException {
+        return concreteSessionFactory;
+    }
+    //jdbc код
+
 
 
     public static Connection getConnection() {
